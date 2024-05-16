eead18329d9ac15d5eb8fe561b4ec4e91a1081c0
liyuefu <yuefuli@gmail.com>
2024-05-17 09:53:13 +1000

    comment on t1.sh

--- t1.sh
+++ t1.sh
@@ -7,4 +7,4 @@ source ./lib/autodglib.sh
 #makeup_file_convert './tmp/dbpath.txt' '/u03/oradata/orcldg' './tmp/pri_convert.txt' './tmp/dg_convert.txt'
 makeup_convert_oneline '../tmp/nofile-pri_convert.txt' '../tmp/pri_convert_oneline.txt'
 
-
+#this test
